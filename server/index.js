278ede39325c5a96d491a42a4035f13c4a9ab380
SeregaBoss <seregaboss@Irinas-MacBook-Air.local>
2025-07-12 17:05:50 -0400

    fix: serve react app and add catch-all route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const getBudgetData = require('./notion');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/api', (req, res) => {
 	res.json({ message: "Hello from server!" });
@@ -17,6 +21,12 @@ app.get('/budgetData', async (req, res) => {
 	
 	const data = await getBudgetData(options);
 	res.json(data);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
